2bc03e35ebda24f259ec9d1563fef311eeb06427
Christian Ramelow <info@codekandis.net>
2023-10-18 18:14:13 +0200

    Adds missing type hints.

--- src/ConstantsClassesTranslatorInterface.php
+++ src/ConstantsClassesTranslatorInterface.php
@@ -10,10 +10,10 @@ interface ConstantsClassesTranslatorInterface
 {
 	/**
 	 * Translates a constants class value into the corresponding constants class value.
-	 * @param mixed $value The constants class value to translate.
-	 * @return mixed The translated corresponding constants class value.
+	 * @param null|bool|int|float|string|array $value The constants class value to translate.
+	 * @return null|bool|int|float|string|array The translated corresponding constants class value.
 	 * @throws CorrespondingConstantsClassValueNotFoundException The constants class value has no corresponding constants class value.
 	 * @throws ConstantsClassValueNotFoundException The constants class value does not exist.
 	 */
-	public function translate( $value );
+	public function translate( null | bool | int | float | string | array $value ): null | bool | int | float | string | array;
 }
