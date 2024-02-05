5c74197a46b0ccce7dd1fcc542d79fcbe19ed964
Christian Ramelow <info@codekandis.net>
2024-02-05 23:15:27 +0100

    Refactors the exception handling.

--- src/ConstantsClassValueNotFoundException.php
+++ src/ConstantsClassValueNotFoundException.php
@@ -1,11 +1,32 @@
 <?php declare( strict_types = 1 );
 namespace CodeKandis\ConstantsClassesTranslator;
 
+use CodeKandis\Types\ConstantNotFoundException;
+use function sprintf;
+
 /**
  * Represents an exception if a constants class value does not exist.
  * @package codekandis/constants-classes-translator
  * @author Christian Ramelow <info@codekandis.net>
  */
-class ConstantsClassValueNotFoundException extends ConstantsClassesTranslatorException
+class ConstantsClassValueNotFoundException extends ConstantNotFoundException implements ConstantsClassValueNotFoundExceptionInterface
 {
+	/**
+	 * Represents the exception message if a constants class value does not exist.
+	 * @var string
+	 */
+	public const string EXCEPTION_MESSAGE_CONSTANTS_CLASS_VALUE_NOT_FOUND = 'The constants class value `%s::%s` does not exist.';
+
+	/**
+	 * Static constructor method.
+	 * @param string $className The class name of the constants class.
+	 * @param null|bool|int|float|string|array $unknownValue The unknown constants class value.
+	 * @return static
+	 */
+	public static function with_classNameAndUnknownValue( string $className, null|bool|int|float|string|array $unknownValue ): static
+	{
+		return new static(
+			sprintf( static::EXCEPTION_MESSAGE_CONSTANTS_CLASS_VALUE_NOT_FOUND, $className, $unknownValue )
+		);
+	}
 }
--- src/ConstantsClassesTranslatorInterface.php
+++ src/ConstantsClassesTranslatorInterface.php
@@ -12,8 +12,8 @@ interface ConstantsClassesTranslatorInterface
 	 * Translates a constants class value into the corresponding constants class value.
 	 * @param null|bool|int|float|string|array $value The constants class value to translate.
 	 * @return null|bool|int|float|string|array The translated corresponding constants class value.
-	 * @throws CorrespondingConstantsClassValueNotFoundException The constants class value has no corresponding constants class value.
-	 * @throws ConstantsClassValueNotFoundException The constants class value does not exist.
+	 * @throws ConstantsClassValueNotFoundExceptionInterface The constants class value does not exist.
+	 * @throws CorrespondingConstantsClassValueNotFoundExceptionInterface The constants class value has no corresponding constants class value.
 	 */
 	public function translate( null|bool|int|float|string|array $value ): null|bool|int|float|string|array;
 }
